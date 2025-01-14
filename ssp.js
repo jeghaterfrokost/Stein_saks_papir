be750864c924eba2e0dedce34d5b2d189fd81a82
jeghaterfrokost <jeppe.brnic.blomquist@gmail.com>
2025-01-14 12:09:59 +0100

    liten endring her og

--- ssp.js
+++ ssp.js
@@ -1,59 +1,57 @@
 let botValg = "";
-let spillerValg = "";
-const ResultatElm = document.getElementById("resultat")
-const botElm = document.getElementById("bot")
-const valg = ["stein", "saks", "papir"];
-const steinElm = document.getElementById("stein")
-const saksElm = document.getElementById("saks")
-const papirElm = document.getElementById("papir")
-const ktElm = document.getElementById("knapptittel")
-const restart = document.getElementById("restart")
-
-function botenhet() {
-    const tilfeldigenhet = Math.floor(Math.random() * 3)
-    botValg = valg[tilfeldigenhet]
-    botElm.innerHTML = botValg.toUpperCase()
-    console.log ("Maskinen valgte", botValg)
-    sjekkresultat()
-}
-
-function velgenhet(valgt) {
-    spillerValg = valgt;
-    console.log ("Spilleren valgte", spillerValg)
-    if (valgt === 'stein') {
-        saksElm.style.display = "none";
-        papirElm.style.display = "none";
-        steinElm.style.display = "block";
-    } else if (valgt === 'saks') {
-        steinElm.style.display = "none";
-        papirElm.style.display = "none";
-        saksElm.style.display = "block"
-    } else if (valgt === 'papir') {
-        steinElm.style.display = "none";
-        saksElm.style.display = "none";
-        papirElm.style.display = "block"
-    }
-}
-
-function sjekkresultat(){
-    if (spillerValg === botValg){
-        ResultatElm.innerHTML = "Det ble uavgjort! Begge valgte " + spillerValg
-    }
-    else if(
-        (spillerValg === 'stein' && botValg === "saks") ||
-        (spillerValg === 'saks' && botValg === "papir") ||
-        (spillerValg === 'papir' && botValg === "stein") 
-    )
-        {
-        ResultatElm.innerHTML = "Du vant!"
-        restart.style.display = "block"
-    }
-    else{
-        ResultatElm.innerHTML = "Du tapte"
-        restart.style.display = "block"
-    }
-}
-
-function restart(){
-    location.reload()
-}
+        let spillerValg = "";
+        const ResultatElm = document.getElementById("resultat")
+        const botElm = document.getElementById("bot")
+        const valg = ["stein", "saks", "papir"];
+        const steinElm = document.getElementById("stein")
+        const saksElm = document.getElementById("saks")
+        const papirElm = document.getElementById("papir")
+        const ktElm = document.getElementById("knapptittel")
+        const restartknapp = document.getElementById("restart")
+ 
+        function botenhet() {
+            const tilfeldigenhet = Math.floor(Math.random() * 3)
+            botValg = valg[tilfeldigenhet]
+            botElm.innerHTML = botValg.toUpperCase()
+            console.log("Maskinen valgte", botValg)
+            sjekkresultat()
+        }
+ 
+        function velgenhet(valgt) {
+            spillerValg = valgt;
+            console.log("Spilleren valgte", spillerValg)
+            if (valgt === 'stein') {
+                saksElm.style.display = "none";
+                papirElm.style.display = "none";
+                steinElm.style.display = "flex";
+            } else if (valgt === 'saks') {
+                steinElm.style.display = "none";
+                papirElm.style.display = "none";
+                saksElm.style.display = "flex"
+            } else if (valgt === 'papir') {
+                steinElm.style.display = "none";
+                saksElm.style.display = "none";
+                papirElm.style.display = "flex"
+            }
+        }
+ 
+        function sjekkresultat() {
+            if (spillerValg === botValg) {
+                ResultatElm.innerHTML = "Det ble uavgjort! Begge valgte " + spillerValg
+            }
+            else if (
+                (spillerValg === 'stein' && botValg === "saks") ||
+                (spillerValg === 'saks' && botValg === "papir") ||
+                (spillerValg === 'papir' && botValg === "stein")
+            ) {
+                ResultatElm.innerHTML = "Du vant!"
+                restartknapp.style.display = "flex"
+            }
+            else {
+                ResultatElm.innerHTML = "Du tapte"
+                restartknapp.style.display = "flex"
+            }
+        }
+ 
+        function restart() {
+            location.reload()
