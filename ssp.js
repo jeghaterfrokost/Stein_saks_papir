d8a25ed42d8b8c7eb8ec3650bf826dfceceab0e5
jeghaterfrokost <jeppe.brnic.blomquist@gmail.com>
2025-01-14 12:02:36 +0100

    et par små endringer

--- ssp.js
+++ ssp.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let botValg = "";
 let spillerValg = "";
 const ResultatElm = document.getElementById("resultat")
@@ -8,6 +7,7 @@ const steinElm = document.getElementById("stein")
 const saksElm = document.getElementById("saks")
 const papirElm = document.getElementById("papir")
 const ktElm = document.getElementById("knapptittel")
+const restart = document.getElementById("restart")
 
 function botenhet() {
     const tilfeldigenhet = Math.floor(Math.random() * 3)
@@ -46,62 +46,14 @@ function sjekkresultat(){
     )
         {
         ResultatElm.innerHTML = "Du vant!"
+        restart.style.display = "block"
     }
     else{
         ResultatElm.innerHTML = "Du tapte"
-    }
-}
-=======
-let botValg = "";
-let spillerValg = "";
-const ResultatElm = document.getElementById("resultat")
-const botElm = document.getElementById("bot")
-const valg = ["stein", "saks", "papir"];
-const steinElm = document.getElementById("stein")
-const saksElm = document.getElementById("saks")
-const papirElm = document.getElementById("papir")
-const ktElm = document.getElementById("knapptittel")
-
-function botenhet() {
-    const tilfeldigenhet = Math.floor(Math.random() * 3)
-    botValg = valg[tilfeldigenhet]
-    botElm.innerHTML = botValg.toUpperCase()
-    console.log ("Maskinen valgte", botValg)
-    sjekkresultat()
-}
-
-function velgenhet(valgt) {
-    spillerValg = valgt;
-    console.log ("Spilleren valgte", spillerValg)
-    if (valgt === 'stein') {
-        saksElm.style.display = "none";
-        papirElm.style.display = "none";
-        steinElm.style.display = "block";
-    } else if (valgt === 'saks') {
-        steinElm.style.display = "none";
-        papirElm.style.display = "none";
-        saksElm.style.display = "block"
-    } else if (valgt === 'papir') {
-        steinElm.style.display = "none";
-        saksElm.style.display = "none";
-        papirElm.style.display = "block"
+        restart.style.display = "block"
     }
 }
 
-function sjekkresultat(){
-    if (spillerValg === botValg){
-        ResultatElm.innerHTML = "Det ble uavgjort! Begge valgte " + spillerValg
-    }
-    else if(
-        (spillerValg === 'stein' && botValg === "saks") ||
-        (spillerValg === 'saks' && botValg === "papir") ||
-        (spillerValg === 'papir' && botValg === "stein") 
-    )
-        {
-        ResultatElm.innerHTML = "Du vant!"
-    }
-    else{
-        ResultatElm.innerHTML = "Du tapte"
-    }
+function restart(){
+    location.reload()
 }
->>>>>>> a0f5e5fc12e049576f22e52e6247250f55cf4d5a
